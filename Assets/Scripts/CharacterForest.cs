36c9e72bc2bf72fa211811bcf401e4be2c77f316
Arnvvd <pinot.arnaud@gmail.com>
2018-05-16 02:15:45 +0200

    Add wheel collider

---
+++ Assets/Scripts/CharacterForest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterForest : MonoBehaviour
+{
+	public Transform path;
+	private List<Transform> nodes;
+	private int currentNode = 0;
+
+	private void Start()
+	{
+		var pathTransforms = path.GetComponentsInChildren<Transform>();
+		nodes = new List<Transform>();
+
+		for (var i = 0; i < pathTransforms.Length; i++)
+		{
+			if (pathTransforms[i] != path.transform)
+			{
+				nodes.Add(pathTransforms[i]);
+			}
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		Move();
+	}
+
+	private void Move()
+	{
+		
+	}
+}
