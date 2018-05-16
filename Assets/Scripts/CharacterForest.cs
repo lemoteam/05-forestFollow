c4226e6104f4b071155bf011cb2a7db6ac51f1a5
Arnvvd <pinot.arnaud@gmail.com>
2018-05-16 03:42:26 +0200

    Displacement on path

--- Assets/Scripts/CharacterForest.cs
+++ Assets/Scripts/CharacterForest.cs
@@ -6,7 +6,8 @@ public class CharacterForest : MonoBehaviour
 {
 	public Transform path;
 	public float maxSteerAngle = 45f; 
-	public WheelCollider wheel;
+	public WheelCollider wheelL;
+	public WheelCollider wheelR;
 	private List<Transform> nodes;
 	private int currentNode = 0;
 
@@ -28,18 +29,40 @@ public class CharacterForest : MonoBehaviour
 	{
 		ApplySteer();
 		Move();
+		CheckWaypointDistance();
 	}
 
 	private void Move()
 	{
-		
+		wheelL.motorTorque = 20f;
+		wheelR.motorTorque = 20f;
 	}
 
 	private void ApplySteer()
 	{
 		var relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
 		var wheelAngle = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
-		wheel.steerAngle = wheelAngle;
-		Debug.Log(wheelAngle);
+		wheelL.steerAngle = wheelAngle;
+		wheelR.steerAngle = wheelAngle;
+		//Debug.Log(wheelAngle);
+	}
+
+	private void CheckWaypointDistance()
+	{
+		//Debug.Log(Vector3.Distance(transform.position, nodes[currentNode].position).ToString());
+
+		if (Vector3.Distance(transform.position, nodes[currentNode].position) < 0.6f)
+		{
+			if (currentNode == nodes.Count - 1)
+			{
+				currentNode = 0;
+				Debug.Log(currentNode);
+			}
+			else
+			{
+				currentNode++;
+				Debug.Log(currentNode);
+			}
+		}
 	}
 }
