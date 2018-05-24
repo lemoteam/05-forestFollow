b0d118a2d28129957fb1a855cb52b595ba25b800
Arnvvd <pinot.arnaud@gmail.com>
2018-05-24 02:54:04 +0200

    Add easing lerp

--- Assets/Scripts/ElementForest.cs
+++ Assets/Scripts/ElementForest.cs
@@ -11,7 +11,8 @@ public class ElementForest : MonoBehaviour {
 	private bool isCurrentPositionSet;
 	
 	private float gemReplaceFraction = 0;
-	private float gemReplaceSpeed = 5f;
+	private float gemReplaceSpeed = 1f;
+	
 
 	private void Start()
 	{
@@ -29,7 +30,7 @@ public class ElementForest : MonoBehaviour {
 		{			
 			if (!(gemReplaceFraction < 1)) return;
 			gemReplaceFraction += Time.deltaTime * gemReplaceSpeed;
-			transform.position = Vector3.Lerp(currentPosition, destinationPosition, gemReplaceFraction);
+			transform.position = new Vector3(transform.position.x, Mathf.SmoothStep(currentPosition.y, destinationPosition.y, CubicEaseOut(gemReplaceFraction)), transform.position.z);
 			
 		} else {
 			
@@ -38,9 +39,9 @@ public class ElementForest : MonoBehaviour {
 				isCurrentPositionSet = true;
 			}
 			
-			if (!(gemReplaceFraction < 1)) return;
+			if (!(gemReplaceFraction < 1));
 			gemReplaceFraction += Time.deltaTime * gemReplaceSpeed;
-			transform.position = Vector3.Lerp(currentPosition, elementPosition, gemReplaceFraction);
+			transform.position = new Vector3(transform.position.x, Mathf.SmoothStep(currentPosition.y, elementPosition.y, CubicEaseOut(gemReplaceFraction)), transform.position.z);
 		}
 	}
 
@@ -58,5 +59,37 @@ public class ElementForest : MonoBehaviour {
 	public void unactiveAnimation() {
 		isActive = false;
 		gemReplaceFraction = 0;
+	}
+	
+	
+	
+	// EASING FUNCTIONS
+	// https://github.com/acron0/Easings/blob/master/Easings.cs
+	
+	static public float CubicEaseOut(float p)
+	{
+		float f = (p - 1);
+		return f * f * f + 1;
+	}
+	
+
+	static public float BounceEaseOut(float p)
+	{
+		if(p < 4/11.0f)
+		{
+			return (121 * p * p)/16.0f;
+		}
+		else if(p < 8/11.0f)
+		{
+			return (363/40.0f * p * p) - (99/10.0f * p) + 17/5.0f;
+		}
+		else if(p < 9/10.0f)
+		{
+			return (4356/361.0f * p * p) - (35442/1805.0f * p) + 16061/1805.0f;
+		}
+		else
+		{
+			return (54/5.0f * p * p) - (513/25.0f * p) + 268/25.0f;
+		}
 	}
 }
